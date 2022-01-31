4eb034b5d18616752a8ba3333b39ed2d83f22d87
Rachel <rachelreillydev@gmail.com>
2022-01-31 09:14:58 -0600

    sets up Color class

---
+++ src/types/Color.ts
@@ -0,0 +1,48 @@
+export interface HslColorType {
+  h: number
+  s: number
+  l: number
+  a?: number
+}
+
+type ColorValueType = HslColorType // Add other color types here
+
+export interface ColorPropType {
+  color: ColorValueType
+  variant1: ColorValueType // Light for HSL, but could be different for RGB
+  variant2: ColorValueType // Dark for HSL, but could be different for RGB
+  text?: '#000' // Recommended text color (#000 or #fff)
+}
+
+export type ColorSetType = ColorPropType[]
+
+export type SetType = 'complementary' | 'triadic' | 'analogous'
+
+export type ColorStringType = string // How can this be validated??
+
+export class Color {
+  get(color: ColorStringType): ColorPropType {
+    // Validates string as specified color string
+    // Turns color into color format
+    // Calls getColorVariants and _getTextColor
+  }
+
+  getRandom(): ColorPropType {
+    // Gets random color
+    // Calls getColorVariants and _getTextColor
+  }
+
+  getColorVariants(color: ColorValueType): ColorPropType {
+    // gets similar variants
+  }
+
+  _getTextColor(color: ColorValueType): string {
+    // TODO: Base this on value of color
+    return '#000'
+  }
+
+  getColorSet(type: SetType, color?: ColorStringType): ColorSetType {
+    // If no color, generate random color
+    // Otherwise convert into color object
+  }
+}
