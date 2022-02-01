5c0574d995e8d51c9b7acb3146fc8aeff75e55c6
Rachel <rachelreillydev@gmail.com>
2022-02-01 11:45:17 -0600

    sets up Node server for index.html

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-export const hello = 'Hello Typescript'
+import http from 'http'
+import fs from 'fs'
+
+const server = http.createServer((_: any, res: any) => {
+  res.writeHead(200, { 'content-type': 'text/html' })
+  fs.createReadStream('index.html').pipe(res)
+})
+
+const PORT = process.env.PORT || 8880
+
+server.listen(PORT, () =>
+  console.log(`Server started on port http://localhost:${PORT}`)
+)
