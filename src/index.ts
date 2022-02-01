b3c3a027bb1cabdd00da90a6b6ce80fa8a379205
Rachel <rachelreillydev@gmail.com>
2022-02-01 16:25:20 -0600

    tries shorter timeout for deploy

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,10 @@
 import { setColors } from './colors'
 
 export function main() {
-  console.log('LOADED MY INDEX SCRIPT AND RAN MAIN')
   const div: HTMLElement | null = document.getElementById('sq_wrapper')
-  console.log('THIS IS MY DIV THAT IS NOT GETTING AN EVENT LISTENER', div)
   if (div) {
-    console.log('ADDED EVENT LISTNENER')
     div.addEventListener('click', setColors)
-  } else setTimeout(main, 10000)
+  } else setTimeout(main, 500)
 }
 
 main()
