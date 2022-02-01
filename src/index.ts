d46343a0da2973105f360b421a3253d08265037a
Rachel <rachelreillydev@gmail.com>
2022-02-01 16:18:29 -0600

    adds log of div before conditional

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ export function main() {
   console.log('LOADED MY INDEX SCRIPT AND RAN MAIN')
   setColors()
   const div: HTMLElement | null = document.getElementById('sq_wrapper')
+  console.log('THIS IS MY DIV THAT IS NOT GETTING AN EVENT LISTENER', div)
   if (div) {
     console.log('ADDED EVENT LISTNENER')
     div.addEventListener('click', setColors)
