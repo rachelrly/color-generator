4468ab8fd625b3ee7c12677883bc3373b7df7983
Rachel <rachelreillydev@gmail.com>
2022-02-01 16:20:42 -0600

    adds timeout that recurses and calls main

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,12 @@ import { setColors } from './colors'
 
 export function main() {
   console.log('LOADED MY INDEX SCRIPT AND RAN MAIN')
-  setColors()
   const div: HTMLElement | null = document.getElementById('sq_wrapper')
   console.log('THIS IS MY DIV THAT IS NOT GETTING AN EVENT LISTENER', div)
   if (div) {
     console.log('ADDED EVENT LISTNENER')
     div.addEventListener('click', setColors)
-  }
+  } else setTimeout(main, 10000)
 }
 
 main()
