e06b475e69bec02e5c70a47cda13ef4314c15054
Rachel <rachelreillydev@gmail.com>
2022-02-01 16:27:04 -0600

    sets initial color before event listener

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { setColors } from './colors'
 export function main() {
   const div: HTMLElement | null = document.getElementById('sq_wrapper')
   if (div) {
+    setColors() // initial color
     div.addEventListener('click', setColors)
   } else setTimeout(main, 500)
 }
