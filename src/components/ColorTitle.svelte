20b01443b934f29414f1d01792cf1a3c9be7873f
Rachel <rachelreillydev@gmail.com>
2022-02-17 16:20:12 -0600

    removes unnecessary conditional

--- src/components/ColorTitle.svelte
+++ src/components/ColorTitle.svelte
@@ -6,20 +6,16 @@
 
 <div class="flex flex-row items-center justify-between w-full max-w-screen-sm">
   <h3 class="text-gray-400 text-center">
-    {color ? `Current color is ${getColorString(color)}` : 'Loading...'}
+    Current color is {getColorString(color)}
   </h3>
   <div>
     <svg
       class="mx-2 flex-1 block"
-      width={50}
-      height={50}
+      width={30}
+      height={30}
       xmlns="http://www.w3.org/2000/svg"
     >
-      <rect
-        height={50}
-        width={50}
-        fill={color ? getColorString(color) : 'none'}
-      />
+      <rect height={30} width={30} fill={getColorString(color)} />
     </svg>
   </div>
 </div>
